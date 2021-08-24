e22971a99e5a62b01364e6965d5e1b90736ac7fb
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-08-23 21:17:42 -0300

    Enum Deploy - Cria Enum's

---
+++ SistemaPedidosEF/Enums/TipoFrete.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaPedidosEF.Enums
+{
+    public enum TipoFrete
+    {
+        CIF,
+        FOB,
+        SemFrete
+    }
+}
---
+++ SistemaPedidosEF/Enums/TipoProduto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaPedidosEF.Enums
+{
+    public enum TipoProduto
+    {
+        MercadoriaParaRevenda,
+        Embalagem,
+        Servico
+    }
+}
