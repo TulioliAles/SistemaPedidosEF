745f653568429842194288ed458db35e57157d58
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-08-23 21:46:26 -0300

    DbContext Deploy - Cria Classe de Contexto e método OnConfiguring

---
+++ SistemaPedidosEF/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaPedidosEF.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source=(localdb)\\ServidorLocal; Initial Catalog=SistemaPedidoEF; Integrated Security=true");
+        }
+    }
+}
