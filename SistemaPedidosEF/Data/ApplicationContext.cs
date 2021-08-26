13b7cb4ac8126b375018c8ccf66e1fc92b2f8bef
Alessandro Tulioli <alessandro.smarapd@hotmail.com>
2021-08-25 21:27:06 -0300

    Mapping Deploy - Cria classes de configuração para mapeamento

--- SistemaPedidosEF/Data/ApplicationContext.cs
+++ SistemaPedidosEF/Data/ApplicationContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SistemaPedidosEF.Domain;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,9 +8,16 @@ namespace SistemaPedidosEF.Data
 {
     public class ApplicationContext : DbContext
     {
+        public DbSet<Pedido> Pedidos { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data source=(localdb)\\ServidorLocal; Initial Catalog=SistemaPedidoEF; Integrated Security=true");
+            optionsBuilder.UseSqlServer("Server=(localdb)\\ServidorLocal; Database=SistemaPedidoEF; Trusted_Connection=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationContext).Assembly);
         }
     }
 }
